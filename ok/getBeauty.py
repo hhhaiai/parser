21ae583faa0e9150c44a913f4fe57bade7ce72cc
二大爷 <sanbo.xyz@gmail.com>
2017-01-02 22:32:21 +0800

    update log

--- ok/getBeauty.py
+++ ok/getBeauty.py
@@ -8,13 +8,12 @@
 '''
 from bs4 import BeautifulSoup  
 import urllib2  
-import os.path
+import os
 from __builtin__ import str
 
 '''
     解析高清地址,并且去下载
 '''
-
 
 def test(i, urls):
     urls = ['http://www.4j4j.cn/beauty/20533.html', 'http://www.4j4j.cn/beauty/20532.html', 'http://www.4j4j.cn/beauty/20528.html', 'http://www.4j4j.cn/beauty/20525.html', 'http://www.4j4j.cn/beauty/20589.html', 'http://www.4j4j.cn/beauty/20524.html', 'http://www.4j4j.cn/beauty/20523.html', 'http://www.4j4j.cn/beauty/20521.html', 'http://www.4j4j.cn/beauty/20518.html', 'http://www.4j4j.cn/beauty/20515.html', 'http://www.4j4j.cn/beauty/20513.html', 'http://www.4j4j.cn/beauty/20511.html', 'http://www.4j4j.cn/beauty/20508.html', 'http://www.4j4j.cn/beauty/20582.html', 'http://www.4j4j.cn/beauty/20504.html', 'http://www.4j4j.cn/beauty/20502.html', 'http://www.4j4j.cn/beauty/20501.html', 'http://www.4j4j.cn/beauty/20498.html', 'http://www.4j4j.cn/beauty/20496.html', 'http://www.4j4j.cn/beauty/20492.html', 'http://www.4j4j.cn/beauty/20489.html', 'http://www.4j4j.cn/beauty/20488.html', 'http://www.4j4j.cn/beauty/20487.html', 'http://www.4j4j.cn/beauty/20628.html', 'http://www.4j4j.cn/beauty/20627.html', 'http://www.4j4j.cn/beauty/20480.html', 'http://www.4j4j.cn/beauty/20479.html', 'http://www.4j4j.cn/beauty/20476.html', 'http://www.4j4j.cn/beauty/20474.html', 'http://www.4j4j.cn/beauty/20610.html', 'http://www.4j4j.cn/beauty/20471.html', 'http://www.4j4j.cn/beauty/20469.html', 'http://www.4j4j.cn/beauty/20466.html', 'http://www.4j4j.cn/beauty/20462.html', 'http://www.4j4j.cn/beauty/20460.html', 'http://www.4j4j.cn/beauty/20457.html', 'http://www.4j4j.cn/beauty/20455.html', 'http://www.4j4j.cn/beauty/20452.html', 'http://www.4j4j.cn/beauty/20449.html', 'http://www.4j4j.cn/beauty/20444.html', 'http://www.4j4j.cn/beauty/20443.html', 'http://www.4j4j.cn/beauty/20441.html', 'http://www.4j4j.cn/beauty/20440.html', 'http://www.4j4j.cn/beauty/20643.html', 'http://www.4j4j.cn/beauty/20642.html', 'http://www.4j4j.cn/beauty/20641.html', 'http://www.4j4j.cn/beauty/20640.html', 'http://www.4j4j.cn/beauty/20639.html', 'http://www.4j4j.cn/beauty/20638.html', 'http://www.4j4j.cn/beauty/20637.html', 'http://www.4j4j.cn/beauty/20636.html', 'http://www.4j4j.cn/beauty/20635.html', 'http://www.4j4j.cn/beauty/20633.html', 'http://www.4j4j.cn/beauty/20632.html', 'http://www.4j4j.cn/beauty/20630.html', 'http://www.4j4j.cn/beauty/20629.html', 'http://www.4j4j.cn/beauty/20624.html', 'http://www.4j4j.cn/beauty/20623.html', 'http://www.4j4j.cn/beauty/20622.html', 'http://www.4j4j.cn/beauty/20620.html', 'http://www.4j4j.cn/beauty/20618.html', 'http://www.4j4j.cn/beauty/20614.html', 'http://www.4j4j.cn/beauty/20613.html', 'http://www.4j4j.cn/beauty/20612.html', 'http://www.4j4j.cn/beauty/20611.html', 'http://www.4j4j.cn/beauty/20608.html', 'http://www.4j4j.cn/beauty/20607.html', 'http://www.4j4j.cn/beauty/20606.html', 'http://www.4j4j.cn/beauty/20605.html', 'http://www.4j4j.cn/beauty/20603.html', 'http://www.4j4j.cn/beauty/20600.html', 'http://www.4j4j.cn/beauty/20599.html', 'http://www.4j4j.cn/beauty/20597.html', 'http://www.4j4j.cn/beauty/20596.html', 'http://www.4j4j.cn/beauty/20595.html', 'http://www.4j4j.cn/beauty/20594.html', 'http://www.4j4j.cn/beauty/20593.html', 'http://www.4j4j.cn/beauty/20592.html', 'http://www.4j4j.cn/beauty/20591.html', 'http://www.4j4j.cn/beauty/20585.html']
@@ -23,7 +22,6 @@ def test(i, urls):
 
 def process(url):
     try:
-        print 'process ' + url
         header = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
             'Cookie': 'AspxAutoDetectCookieSupport=1',
@@ -75,8 +73,8 @@ def download(url):
         request = urllib2.Request(url, None, header)
         content = urllib2.urlopen(request, data=None, timeout=3).read()
         filename = path + str(number) + '.jpg'  
-    #    print 'download [' + filename + ']===>' + url
-        print 'download [' + filename + ']'
+        print 'download [' + filename + ']===>' + url
+        #print 'download [' + filename + ']'
         f = open(filename, 'wb') #这种模式可以正常下载图片
         f.write(content)
         f.flush()
@@ -85,7 +83,7 @@ def download(url):
         print  str(e)
       
 if __name__ == '__main__':
-    number = 1
+    number = 0
     path = 'D:/girls/'
     #如果没有文件夹则创建
     if not  os.path.exists(path):
