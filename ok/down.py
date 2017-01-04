3e468efd02852cea0561e7e8380fe506ad3bf3bb
二大爷 <sanbo.xyz@gmail.com>
2017-01-04 23:48:16 +0800

    更新下文件.

--- ok/down.py
+++ ok/down.py
@@ -14,29 +14,23 @@ from bs4 import BeautifulSoup
               2015.6.2  发现被禁止抓取了，增加了随机的浏览器头部信息，模拟浏览器抓取。
 '''
 
-'''
-获取网站的soup对象
-'''
+#获取网站的soup对象
 def get_soup(url):
     my_headers = ['Mozilla/5.0 (Windows NT 5.2) AppleWebKit/534.30 (KHTML, like Gecko) Chrome/12.0.742.122 Safari/534.30', 'Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0', 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.2; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET4.0E; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET4.0C)', 'Opera/9.80 (Windows NT 5.1; U; zh-cn) Presto/2.9.168 Version/11.50', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1', 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.04506.648; .NET CLR 3.5.21022; .NET4.0E; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET4.0C)']
     header = {"User-Agent":random.choice(my_headers)}
     req = urllib2.Request(url, headers=header)
     html = urllib2.urlopen(req).read()
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html, "lxml")
     return soup
 
-'''
-获取妹子图网站的页数
-'''
+#获取妹子图网站的页数
 def get_pages(url):
     soup = get_soup(url)
     nums = soup.find_all('a', class_='page-numbers')
     pages = int(nums[-2].text)
     return pages
 
-'''
-获取页面的所有妹子图主题的链接名称和地址，记入列表
-'''
+#获取页面的所有妹子图主题的链接名称和地址，记入列表
 def get_menu(url):
     soup = get_soup(url)
     menu = []
@@ -49,9 +43,7 @@ def get_menu(url):
             menu.append([name, address])
     return menu
 
-'''
-获取单个妹子图主题一共具有多少张图片
-'''
+#获取单个妹子图主题一共具有多少张图片
 def get_links(url):
     soup = get_soup(url)
     all_ = soup.find_all('a')
@@ -62,49 +54,40 @@ def get_links(url):
             nums.append(span[0].text)
     return nums[-2]
 
-'''
-从单独的页面中提取出图片保存为filename
-'''
+#从单独的页面中提取出图片保存为filename
 def get_image(url, filename):
     soup = get_soup(url)
     image = soup.find_all('p')[0].find_all('img')[0]['src']
     urllib.urlretrieve(image, filename)
 
-'''
-下载第page页的妹子图
-'''
+#下载第page页的妹子图
 def main(page):
-    print u'正在下载第 %d 页' % page
+    print (u'正在下载第 %d 页' % page)
     page_url = url + '/page/' + str(page)
     menu = get_menu(page_url)
-    print u'@@@@@@@@@@@@@@@@第 %d 页共有 %d 个主题@@@@@@@@@@@@@@@@' % (page, len(menu))
+    print (u'@@@@@@@@@@@@@@@@第 %d 页共有 %d 个主题@@@@@@@@@@@@@@@@' % (page, len(menu)))
     for i in menu:
         dir_name = os.path.join('MeiZiTu', i[0])
         if not os.path.exists(dir_name):
             os.mkdir(dir_name)
         pic_nums = int(get_links(i[1]))
-        print u'\n\n\n*******主题 %s 一共有 %d 张图片******\n' % (i[0], pic_nums)
+        print (u'\n\n*******主题 %s 一共有 %d 张图片******' % (i[0], pic_nums))
         for pic in range(1, pic_nums + 1):
             basename = str(pic) + '.jpg'
             filename = os.path.join(dir_name, basename)
             pic_url = i[1] + '/' + str(pic)
             if not os.path.exists(filename):
-                print u'......%s' % basename,
+                print (u'[■〓%s〓■]∷' % basename),
                 get_image(pic_url, filename)
             else:
-                print filename + u'已存在，略过'
+                print (filename + u'已存在，略过')
 
-"""
-哈哈哈
-"""
+#哈哈哈
 if __name__ == '__main__':
     url = 'http://www.mzitu.com/'
     pages = get_pages(url)
-    print u'***************妹子图一共有 %d 页******************' % pages
+    print (u'***************妹子图一共有 %d 页******************' % pages)
     if not os.path.exists('MeiZiTu'):
-        os.mkdir('MeiZiTu')
-    else:
-        os.remove('MeiZiTu')
         os.mkdir('MeiZiTu')
     page_start = input(u'Input the first page number:\n')
     page_end = input(u'Input the last page number:\n')
@@ -114,4 +97,4 @@ if __name__ == '__main__':
     elif page_end == page_start:
         main(page_end)
     else:
-        print u"输入错误，起始页必须小于等于结束页\n"
+        print (u"输入错误，起始页必须小于等于结束页\n")
